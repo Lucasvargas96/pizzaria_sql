2cb568ffcc1abf8fa31d403d99a2e6f3d472f6e2
Lucasvargas96 <91397748+Lucasvargas96@users.noreply.github.com>
2023-01-15 15:22:29 -0300

    fiz o restante das querys que estruturam o back-end

--- process/pizza.php
+++ process/pizza.php
@@ -26,9 +26,33 @@ if($method === "GET"){
         $_SESSION["status"]="warning";
 
     } else{
+        //validação para pedido com sucesso
         $_SESSION["msg"]= "Pedido realizado com sucesso";
         $_SESSION["status"]="success";
-        
+        //inserindo nas tabelas os dados que vieram do input
+        $stmt = $conn->prepare("INSERT INTO pizzas (borda_id,massa_id) VALUES (:borda,:massa)");
+        $stmt->bindParam(":borda",$borda,PDO::PARAM_INT);
+        $stmt->bindParam(":massa",$massa,PDO::PARAM_INT);
+        $stmt->execute();
+        //inserindo nas tabeleas os dados com os dados dos sabores que vieram do input
+        $pizzaId =$conn->lastInsertId();//esse metodo retorna o ultimo id,(isso vem do auto_increment)
+        $stmt2=$conn->prepare("INSERT INTO pizza_sabor (pizza_id,sabor_id) VALUES (:pizza,:sabor)");
+
+        foreach ($sabores as $sabor) {
+            $stmt2->bindParam(":pizza",$pizzaId, PDO::PARAM_INT);
+            $stmt2->bindParam(":sabor",$sabor,PDO::PARAM_INT);
+            $stmt2->execute();
+            
+        }
+        //criando o pedido da pizza
+        $stmt3 =$conn->prepare("INSERT INTO pedidos (pizza_id,status_id) VALUES (:pizza,:status)");
+
+        $statusId=1;
+
+        $stmt3->bindParam(":pizza",$pizzaId);
+        $stmt3->bindParam(":status",$statusId);
+
+        $stmt3->execute();
     }
     header("Location:..");
 }
